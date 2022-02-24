c3148449b02cca1e0216d41d01b59e2781058b81
dsanteugini <hefesto84@gmail.com>
2022-02-24 19:18:57 +0100

    First Commit

--- indiana-jones-desktop-adventures-ripper/Program.cs
+++ indiana-jones-desktop-adventures-ripper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using indiana_jones_desktop_adventures_ripper.Services;
 
 namespace indiana_jones_desktop_adventures_ripper
 {
@@ -6,7 +8,24 @@ namespace indiana_jones_desktop_adventures_ripper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var areFilesValid = true;
+                
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"File not found, check the path: {args[0]}");
+                areFilesValid = false;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine($"File not found, check the path: {args[1]}");
+                areFilesValid = false;
+            }
+
+            if (!areFilesValid) return;
+            
+            var ripperService = new RipperService(File.OpenRead(args[0]), File.OpenRead(args[1]));
+            ripperService.Rip();
         }
     }
 }
---
+++ indiana-jones-desktop-adventures-ripper/Services/RipperService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace indiana_jones_desktop_adventures_ripper.Services
+{
+    public class RipperService
+    {
+        private readonly FileStream _dataBinaryFileStream;
+        private readonly FileStream _execBinaryFileStream;
+        
+        public RipperService(FileStream dataBinaryFileStream , FileStream execBinaryFileStream)
+        {
+            _dataBinaryFileStream = dataBinaryFileStream;
+            _execBinaryFileStream = execBinaryFileStream;
+        }
+
+        public void Rip()
+        {
+            if (_dataBinaryFileStream == null || _execBinaryFileStream == null)
+                throw new Exception("Error reading data.");
+        }
+    }
+}
