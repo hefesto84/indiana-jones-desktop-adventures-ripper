53fd1300031192fa5ce834aff3d13277ff3f9923
dsanteugini <hefesto84@gmail.com>
2022-02-24 18:49:41 +0100

    First Commit

---
+++ indiana-jones-desktop-adventures-ripper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace indiana_jones_desktop_adventures_ripper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
