5aed07aad263918b07a86887a230a523b0b33230
dsanteugini <hefesto84@gmail.com>
2022-02-25 22:43:09 +0100

    Fixed Transparency

--- indiana-jones-desktop-adventures-ripper/Data/TileData.cs
+++ indiana-jones-desktop-adventures-ripper/Data/TileData.cs
@@ -6,8 +6,6 @@ using indiana_jones_desktop_adventures_ripper.Models;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-using Color = System.Drawing.Color;
 
 namespace indiana_jones_desktop_adventures_ripper.Data
 {
@@ -17,6 +15,9 @@ namespace indiana_jones_desktop_adventures_ripper.Data
         
         private readonly Palette _palette;
         private Dictionary<string, Image> _tiles;
+        private const int SpriteW = 32;
+        private const int SpriteH = 32;
+        private const string SpritesheetFilePath = "Tiles/spritesheet.png";
         
         public TileData(Palette palette)
         {
@@ -43,7 +44,7 @@ namespace indiana_jones_desktop_adventures_ripper.Data
 
                 var img = CreateBitmap(tileData);
                 
-                _tiles.Add($"Tiles/tile_{nTiles}.png", img);
+                _tiles.Add($"tile_{nTiles}.png", img);
                 
                 nTiles++;
             }
@@ -56,17 +57,17 @@ namespace indiana_jones_desktop_adventures_ripper.Data
             var ms = new MemoryStream(data);
             var br = new BinaryReader(ms);
 
-            var img = new Image<Rgba32>(32, 32);
+            var img = new Image<Rgba32>(SpriteW, SpriteH);
             
-            for (var j = 0; j < 1024; j++)
+            for (var j = 0; j < SpriteW*SpriteH; j++)
             {
                 var pixelData = br.ReadByte();
                 var pd = (int) pixelData;
 
                 var pixelColor = _palette.GetColor(pd);
 
-                var x = j % 32;
-                var y = j / 32;
+                var x = j % SpriteW;
+                var y = j / SpriteH;
        
                 img[x,y] = pixelData == 0 ? new Rgba32(255,255,255,0) : new Rgba32(pixelColor.R, pixelColor.G, pixelColor.B, 255);
             }
@@ -76,11 +77,45 @@ namespace indiana_jones_desktop_adventures_ripper.Data
 
         private void SaveSpritesheet()
         {
+            var sq = (int)Math.Round(Math.Sqrt(_tiles.Count));
+            
+            var width = sq * SpriteW;
+            var height = sq * SpriteH;
+            
+            var spriteSheet = new Image<Rgba32>(width, height);
+            
+            var row = 0;
+            var col = 0;
+            var index = 0;
+            
             foreach (var (key, value) in _tiles)
             {
-                value.SaveAsPng(key, new PngEncoder());
-                Console.WriteLine($"Adding {key} to spritesheet");
+                var d = value.CloneAs<Rgba32>();
+
+                for (var x = 0; x < SpriteH; x++)
+                {
+                    for (var y = 0; y < SpriteW; y++)
+                    {
+                        spriteSheet[x+(row*SpriteH), y+(col*SpriteW)] = d[x, y];
+                    }
+                }
+                
+                index ++;
+
+                if (index % sq == 0)
+                {
+                    row = 0;
+                    col++;
+                }
+                else
+                {
+                    row++;
+                }
+
             }
+           
+            
+            spriteSheet.SaveAsPng(SpritesheetFilePath,new PngEncoder());
         }
     }
 }
