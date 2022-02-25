4337aec8a70b4852365cf3472ce3e828b678bb8f
dsanteugini <hefesto84@gmail.com>
2022-02-25 22:49:27 +0100

    Fixed Transparency

--- indiana-jones-desktop-adventures-ripper/Services/RipperService.cs
+++ indiana-jones-desktop-adventures-ripper/Services/RipperService.cs
@@ -29,46 +29,6 @@ namespace indiana_jones_desktop_adventures_ripper.Services
             SetupPalette();
 
             ParseSections();
-            var p = 0;
-            /*
-            Console.WriteLine($"Position {_execBinaryFileStream.BaseStream.Position}");
-            
-            _execBinaryFileStream.BaseStream.Position = _offsetPalette;
-            
-            Console.WriteLine($"Position {_execBinaryFileStream.BaseStream.Position}");
-
-            var b = new byte[256 * 4];
-            */
-
-            /*
-            for (var i = 0; i < b.Length; i++)
-            {
-                var s = _execBinaryFileStream.ReadByte();
-                
-                Console.WriteLine($"Position {_execBinaryFileStream.BaseStream.Position}");
-            }*/
-
-            /*
-            var s = _execBinaryFileStream.ReadBytes(b.Length);
-            
-            Console.WriteLine($"Bytes read: {b.Length}");
-
-            var bs = new BinaryWriter(File.OpenWrite("palette.bin"));
-            bs.Write(s);
-            bs.Flush();
-            bs.Close();
-            */
-            //var s = _execBinaryFileStream.Read(b, (int)_execBinaryFileStream.BaseStream.Position, b.Length-1);
-
-
-            /*
-            var paletteBuffer = new byte[256*4];
-            
-            var result = _execBinaryFileStream.Read(paletteBuffer, _offsetPalette, paletteBuffer.Length);
-            */
-            //Console.WriteLine("RES: "+result);
-            //_execBinaryFileStream.Position = _offsetPalette;
-
         }
 
         private void CheckFileStreams()
