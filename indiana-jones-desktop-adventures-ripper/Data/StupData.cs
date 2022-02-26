170149615cf0f972ac9222ee5f3e29bbbb81bdbc
dsanteugini <hefesto84@gmail.com>
2022-02-26 10:24:06 +0100

    vv0.1

---
+++ indiana-jones-desktop-adventures-ripper/Data/StupData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using indiana_jones_desktop_adventures_ripper.Data.Base;
+using indiana_jones_desktop_adventures_ripper.Models;
+
+namespace indiana_jones_desktop_adventures_ripper.Data
+{
+    public class StupData : IData
+    {
+        public static string Tag => "STUP";
+        
+        public void Parse(Section section)
+        {
+            Console.WriteLine($"Parsing: {Tag}");
+        }
+    }
+}
---
+++ indiana-jones-desktop-adventures-ripper/Models/Palette.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Color = System.Drawing.Color;
+
+namespace indiana_jones_desktop_adventures_ripper.Models
+{
+    public class Palette : FileStreamModel
+    {
+        private const int OffsetPalette = 0x36656;
+        private const int PaletteSize = 1024;
+        private const int PaletteColors = 256;
+
+        private readonly int[] _colors = new int[PaletteColors];
+        
+        public Palette(BinaryReader execBinaryFileStream) : base(execBinaryFileStream) { }
+
+        public void Extract()
+        {
+            Fs.BaseStream.Position = OffsetPalette;
+            
+            var data = Fs.ReadBytes(PaletteSize);
+
+            var ms = new MemoryStream(data);
+            var br = new BinaryReader(ms);
+
+            for (var i = 0; i < PaletteColors; i++)
+            {
+                var entry = br.ReadInt32();
+                _colors[i] = entry;
+            }
+            
+            br.Close();
+            ms.Close();
+        }
+
+        public Color GetColor(int index)
+        {
+            return Color.FromArgb(_colors[index]);
+        }
+    }
+}
